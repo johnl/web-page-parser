4c603aec6e50aec1d700de8c125ff10b99fc3d8b
John Leach <john@johnleach.co.uk>
2009-04-13 15:12:21 +0100

    Renamed base.rb to base_parser.rb

--- lib/web-page-parser.rb
+++ lib/web-page-parser.rb
@@ -1,3 +1,4 @@
 # $:.unshift File.join(File.dirname(__FILE__), 'web-page-parser')
 
-require 'web-page-parser/base.rb'
+require 'web-page-parser/base_parser.rb'
+require 'web-page-parser/base_parser_factory.rb'
