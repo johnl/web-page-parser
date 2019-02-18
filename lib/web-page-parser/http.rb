49e268f46efd92dc41d4a0e28c391b4a1a41e7da
John Leach <john@johnleach.co.uk>
2019-02-18 22:36:01 +0000

    Add Accept-Language header when retrieving
    Should avoid a delay when contacting washpo, and generally looks less like a bot

--- lib/web-page-parser/http.rb
+++ lib/web-page-parser/http.rb
@@ -27,6 +27,7 @@ module WebPageParser
           c.autoreferer = true
           c.headers["User-Agent"] = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.4) Gecko/20060508 Firefox/1.5.0.4'
           c.headers["Accept-encoding"] = 'gzip, deflate'
+          c.headers["Accept-Language"] = 'en-GB,en;q=0.5'
         end
       end
 
